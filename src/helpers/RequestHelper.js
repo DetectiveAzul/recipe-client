2a0699e7de0d6abd45e7f2465597e5f78b979a93
Jaime Lopez <detec.azul@gmail.com>
2018-08-15 15:25:41 +0100

    Refactored RequestHelper to modern class syntax

--- src/helpers/RequestHelper.js
+++ src/helpers/RequestHelper.js
@@ -1,36 +1,40 @@
-const RequestHelper = function (url) {
-  this.url = url;
+class RequestHelper {
+  constructor(url) {
+    this.url = url;
+  };
+
+  get() {
+    return fetch(this.url)
+      .then((response) => response.json());
+  };
+
+  delete(id) {
+    return fetch(`${this.url}/${id}`, {
+      method: 'DELETE'
+    })
+      .then((response) => response.json());
+  };
+
+
+  post(payload) {
+    return fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((response) => response.json());
+  };
+
+  put(id, payload) {
+    return fetch(`${this.url}/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(payload),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((response) => response.json());
+  };
+
 };
 
-RequestHelper.prototype.get = function () {
-  return fetch(this.url)
-    .then((response) => response.json());
-};
 
-RequestHelper.prototype.delete = function (id) {
-  return fetch(`${this.url}/${id}`, {
-    method: 'DELETE'
-  })
-    .then((response) => response.json());
-};
-
-
-RequestHelper.prototype.post = function (payload) {
-  return fetch(this.url, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then((response) => response.json());
-};
-
-RequestHelper.prototype.put = function (id, payload) {
-  return fetch(`${this.url}/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify(payload),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then((response) => response.json());
-};
-
-module.exports = RequestHelper;
+export default RequestHelper;
