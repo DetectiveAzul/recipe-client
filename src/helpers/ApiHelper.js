065667452bdff13134119a7c0418227931f2aa71
Jaime Lopez <detec.azul@gmail.com>
2018-08-17 14:29:47 +0100

    Refactored all API helper to async promises

--- src/helpers/ApiHelper.js
+++ src/helpers/ApiHelper.js
@@ -17,16 +17,44 @@ class ApiHelper {
   };
 
   async delete(id) {
-
+    try {
+      const response = await fetch(`${this.url}/${id}`, {
+        method: 'DELETE'
+      });
+      return await response.json();
+    }
+    catch(error) {
+      return error;
+    }
   };
 
 
   async post(payload) {
-
+    try {
+      const response = await fetch(this.url, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {'Content-Type': 'application/json' }
+      });
+      return await response.json();
+    }
+    catch(error) {
+      return error;
+    }
   };
 
   async put(id, payload) {
-
+    try {
+      const response = await fetch(`${this.url}/${id}`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {'Content-Type': 'application/json' }
+      });
+      return await response.json();
+    }
+    catch(error) {
+      return error;
+    }
   };
 
 };
