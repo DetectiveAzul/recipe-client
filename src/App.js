55fb4507bd1dc35277f57db93c4c82d0a2f666cb
Jaime Lopez <detec.azul@gmail.com>
2018-08-17 14:33:23 +0100

    Tested new ApiHelper file. Working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+// import Request from './helpers/ApiHelper.js';
 
 class App extends Component {
   render() {
+    // const request = new Request('recipes');
+    // request.delete(1)
+    //   .then(res => console.log(res));
+
     return (
       <div className="App">
         <h1>Hello World</h1>
