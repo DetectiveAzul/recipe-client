af12278b049beaea05ae31cb38dbe57a15946201
Jaime Lopez <detec.azul@gmail.com>
2018-08-20 13:48:41 +0100

    Added very basic styiling to Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background: ${props => props.primary ? 'palevioletred' : 'white'};
+  color: ${props => props.primary ? 'white' : 'palevioletred'};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 
 const Recipe = (props) => {
   return(
-    <div className='recipe'>
+    <Wrapper className='recipe'>
       <h3>{props.name}</h3>
       <p>{props.description}</p>
-    </div>
+    </Wrapper>
   );
 };
 
