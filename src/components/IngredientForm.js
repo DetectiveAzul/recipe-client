7d1d2356ab9154575c3fa164dd86c87666ee5873
Jardoe <jussi.g.pardoe@gmail.com>
2018-08-20 15:48:49 +0100

    added styling and normalised data input

--- src/components/IngredientForm.js
+++ src/components/IngredientForm.js
@@ -3,9 +3,14 @@ import React from 'react';
 const IngredientForm = (props) => {
   const handleClick = (event) => {
     event.preventDefault();
+    const name = prettifyData(event.target.name.value)
     props.submit({
-      name: event.target.name.value
+      name: name
     })
+  }
+
+  function prettifyData(data) {
+    return data.replace(/\b\w/g, l => l.toUpperCase()).trim()
   }
 
   return(
