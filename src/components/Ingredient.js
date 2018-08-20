f002c8e6a8db9e2dd680a52b26b52bc035fbeb58
8403463 <a8403463@8403463s-MacBook-Pro.local>
2018-08-20 15:46:55 +0100

    added styling and normalised data input

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -5,6 +5,17 @@ const IngredientWrapper = styled.div`
   display: flex;
 `
 
+const InfoWrapper = styled.div`
+  background: ${props => props.primary ? 'palevioletred' : 'white'};
+  color: ${props => props.primary ? 'white' : 'palevioletred'};
+  flex: 3;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
+
 const Button = styled.button`
   background: ${props => props.primary ? 'palevioletred' : 'white'};
   color: ${props => props.primary ? 'white' : 'palevioletred'};
@@ -23,7 +34,9 @@ const Ingredient = (props) => {
   }
     return(
     <IngredientWrapper>
-      <li>{props.name}</li>
+      <InfoWrapper>
+        <li>{props.name}</li>
+      </InfoWrapper>
       <Button onClick={handleClick}>Delete</Button>
     </IngredientWrapper>
   )
