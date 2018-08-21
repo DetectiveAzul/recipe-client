a54e7f2d21c27810c30b8c2d55ed63d5ff10e61c
Jardoe <jussi.g.pardoe@gmail.com>
2018-08-21 09:08:51 +0100

    added cursor: pointer to recipe and delete button

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -25,6 +25,7 @@ const Button = styled.button`
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
+  cursor: pointer;
 `
 
 const Ingredient = (props) => {
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -15,6 +15,7 @@ const InfoWrapper = styled.div`
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
+  cursor: pointer;
 `;
 
 const Button = styled.button`
@@ -26,6 +27,7 @@ const Button = styled.button`
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
+  cursor: pointer;
 `;
 
 
