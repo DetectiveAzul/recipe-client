fcae3422d5116e1d10489dc7a86fc01a8cbda5f9
Jaime Lopez <detec.azul@gmail.com>
2018-08-28 09:40:24 +0100

    Creating redux store on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//REDUX
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { recipeApp } from './redux/reducers/index.js';
+//APPfiles
+import App from './App.js';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
