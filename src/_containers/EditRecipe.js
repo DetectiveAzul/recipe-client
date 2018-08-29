bd858e81a0c0128aa71aefdeb8b5c767ec066b0f
Jaime Lopez <detec.azul@gmail.com>
2018-08-29 15:17:17 +0100

    Added empty component for the edit form

--- src/_containers/EditRecipe.js
+++ src/_containers/EditRecipe.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { navigate } from '@reach/router';
+// import { navigate } from '@reach/router';
 import { connect } from 'react-redux';
 // import { updateRecipe } from '../redux/actions/index.js';
-import RecipeForm from '../_components/RecipeNew/RecipeForm.js';
+import RecipeForm from '../_components/RecipeEdit/RecipeForm.js';
 
 let EditRecipe = ({ dispatch }) => {
 
-  const editRecipe = (newbody, id) => {
+  // const editRecipe = (newbody, id) => {
     // dispatch(updateRecipe(newBody, id));
     // navigate(`recipes/${id}`)
-  };
+  // };
 
   return(
-    'Hello World'
-    // <RecipeForm editRecipe={editRecipe} />
+    <RecipeForm />
   )
 
 };
