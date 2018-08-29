d78213e3b355d17bbe300af78855a56819156636
Jaime Lopez <detec.azul@gmail.com>
2018-08-29 15:53:59 +0100

    Removing unused references

--- src/_containers/EditRecipe.js
+++ src/_containers/EditRecipe.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import { updateRecipe } from '../redux/actions/index.js';
 import RecipeForm from '../_components/RecipeEdit/RecipeForm.js';
-import ApiHelper from '../_helpers/ApiHelper.js'
 
 let EditRecipe = ({ dispatch }) => {
 
