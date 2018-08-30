3d805aa74482b211be67489f83bc87447ab2c13a
Jaime Lopez <detec.azul@gmail.com>
2018-08-30 10:53:27 +0100

    Adding same structure than new recipe form

--- src/_components/RecipeEdit/RecipeForm.js
+++ src/_components/RecipeEdit/RecipeForm.js
@@ -1,18 +1,65 @@
 import React, {Component} from 'react';
+import fetch from 'cross-fetch';
+import config from '../../_helpers/config.js';
+
 
 class RecipeForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      id: props.id
     };
   };
 
+  componentWillMount() {
+    return fetch(`${config.apiUrl}/recipes/${this.props.id}`)
+      .then(response => response.json())
+      .then((json) => {
+        this.setState({
+          info: json.data.info,
+          ingredients: json.data.ingredients,
+          quantities: json.data.quantities,
+          measurements: json.data.measurements,
+          steps: json.data.steps
+          })
+      });
+  };
+
+  renderNameField() {
+    return <input required type='text' name='name' value={`${this.state.info.name}`} />
+  }
+
+  renderDescriptionField() {
+    return <input type='text' name='description' value={`${this.state.info.description}`} />
+  }
+
   render() {
     return(
-      <form>
+      <fieldset>
+        <legend>Edit Recipe ID {`${this.state.id}`}</legend>
+        <form className='form'>
 
-      </form>
+          <h4>Info</h4>
+          <label>Recipe Name</label>
+            { (this.state.info)?this.renderNameField():""}
+          <br />
+
+          <label>Description</label>
+            { (this.state.info)?this.renderDescriptionField():""}
+          <div className='ingredient-form-section'>
+            <h4>Ingredients</h4>
+              {  }
+          </div>
+
+          <div className='steps-form-section'>
+            <h4>Steps</h4>
+            {  }
+          </div>
+
+          <input type='submit' value='Add Recipe' />
+
+        </form>
+      </fieldset>
   )};
 };
 export default RecipeForm;
--- src/_containers/EditRecipe.js
+++ src/_containers/EditRecipe.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 // import { updateRecipe } from '../redux/actions/index.js';
 import RecipeForm from '../_components/RecipeEdit/RecipeForm.js';
 
-let EditRecipe = ({ dispatch }) => {
+let EditRecipe = (props) => {
 
   const editRecipe = (newbody, id) => {
     //This will dispatch the action to redux
@@ -13,6 +13,7 @@ let EditRecipe = ({ dispatch }) => {
   return(
     <RecipeForm
       editRecipe = {editRecipe}
+      id = {props.id}
     />
   )
 
