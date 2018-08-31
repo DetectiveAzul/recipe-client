0969c977f4748aac09ca7b45d7e9baaed504e0df
Jardoe <jussi.g.pardoe@gmail.com>
2018-08-31 13:55:01 +0100

    removed errors complaining about unique keys, and tbody tags

--- src/_components/Table.js
+++ src/_components/Table.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import uuidv1 from  'uuid/v1';
 
 class Table extends Component{
 
@@ -6,25 +7,27 @@ class Table extends Component{
     var dataColumns = this.props.data.columns;
     var dataRows = this.props.data.rows;
 
-    var tableHeaders = (<thead>
-         <tr>
+    var tableHeaders = (<thead key={uuidv1()}>
+         <tr key={uuidv1()}>
            {dataColumns.map(function(column) {
-             return <th>{column}</th>; })}
+             return <th key={uuidv1()}>{column}</th>; })}
          </tr>
      </thead>);
 
    var tableBody = dataRows.map(function(row) {
      return (
-       <tr>
+       <tr key={uuidv1()}>
          {dataColumns.map(function(column) {
-           return <td>{row[column]}</td>; })}
+           return <td key={uuidv1()}>{row[column]}</td>; })}
        </tr>); });
    return (
      <div>
        <h3>{this.props.name}</h3>
        <table className="table table-bordered table-hover" width="100%">
          {tableHeaders}
+         <tbody>
          {tableBody}
+         </tbody>
         </table>
       </div>
      )
