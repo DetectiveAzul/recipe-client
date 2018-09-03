7911a0c2bd781a7b086b868fcd55138b8c929e75
Jaime Lopez <detec.azul@gmail.com>
2018-09-03 09:20:41 +0100

    Styled h1 and p from conversion guide

--- src/_containers/ConversionGuide.js
+++ src/_containers/ConversionGuide.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Table from '../_components/Table.js'
 import {volume, weight, temperatures} from '../data/conversion'
+//Style
+import {
+  Header, Description
+} from './styles/ConversionGuideStyle.js';
 
  const ConversionGuide = () => {
   return (
     <div>
-      <h1>Conversion Guide</h1>
-      <p>Different measurements are used all over the world. This is an approximate guide to help.</p>
+      <Header>Conversion Guide</Header>
+      <Description>Different measurements are used all over the world. This is an approximate guide to help.</Description>
       <Table data={volume} name={'Volume'}/>
       <Table data={weight} name={'Weight'} />
       <Table data={temperatures} name={'Temperatures'} />
---
+++ src/_containers/styles/ConversionGuideStyle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Header = styled.h1`
+  background-color: rgb(255,250,250, 0.8);
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.5);
+  margin: 15px;
+  padding: 20px;
+  text-align: center;
+`
+export const Description = styled.p`
+  background-color: rgb(255,250,250, 0.8);
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.5);
+  margin: 15px;
+  padding: 20px;
+  text-align: center;
+`
