6a4fb519db2c213e3b96942d8b7f7d32a6935136
Jaime Lopez <detec.azul@gmail.com>
2018-09-03 09:23:55 +0100

    Modifying paddings and margins

--- src/_containers/ConversionGuide.js
+++ src/_containers/ConversionGuide.js
@@ -3,18 +3,18 @@ import Table from '../_components/Table.js'
 import {volume, weight, temperatures} from '../data/conversion'
 //Style
 import {
-  Header, Description
+  Container, Header, Description
 } from './styles/ConversionGuideStyle.js';
 
  const ConversionGuide = () => {
   return (
-    <div>
+    <Container>
       <Header>Conversion Guide</Header>
       <Description>Different measurements are used all over the world. This is an approximate guide to help.</Description>
       <Table data={volume} name={'Volume'}/>
       <Table data={weight} name={'Weight'} />
       <Table data={temperatures} name={'Temperatures'} />
-    </div>
+    </Container>
   )
 }
 
