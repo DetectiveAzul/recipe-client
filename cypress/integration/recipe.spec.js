c08d50faef3e7d50a1b08590b30437ede0721967
Jaime Lopez <detec.azul@gmail.com>
2018-09-14 10:36:01 +0100

    Added cypress tests for rest of navbar buttons.

--- cypress/integration/recipe.spec.js
+++ cypress/integration/recipe.spec.js
@@ -1,9 +1,22 @@
-describe('Recipe Home Page Testing', () => {
-    beforeEach(() => {
-        cy.visit('/');
+describe('Recipe Testing', () => {
+    context('Testing Routing through NavBar', () => {
+        before(() => {
+            cy.visit('/');
+        });
+    
+        it('should enter index after clicking on Recipes', () => {
+            cy.get('Button').contains('Recipes').click();
+            cy.url().should('include', '/recipes');
+        });
+    
+        it('should enter new recipe form after clicking on New', () => {
+            cy.get('Button').contains('New').click();
+            cy.url().should('include', 'recipes/new');
+        });
+    
+        it('should enter conversion guide after clicking on Conversion Guide', () => {
+            cy.get('Button').contains('Conversion Guide').click();
+            cy.url().should('include', '/guide');
+        });    
     });
-
-    it('should visit without problem each page using the navbar', () => {
-        cy.get('Button').contains('Recipes').click();
-    })
 });
